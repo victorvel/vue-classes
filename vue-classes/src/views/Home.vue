3846676ea8860db9827092c5a5c68b71a7e2f6c1
Victor Velasco <victorvelasco098@gmail.com>
2020-11-11 11:16:31 -0700

    set up

---
+++ vue-classes/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <div class="search-bar">
+        <form class="pure-form">
+            <i class="fas fa-search"></i><input v-model="searchText" />
+        </form>
+    </div>
+    <ClassData />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ClassData from '@/components/ClassList.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ClassData
+  },
+  data() {
+    return {
+      searchText: '',
+    }
+  },
+  computed: {
+  }
+}
+</script>
+<style scoped>
+  .search-bar {
+    margin-bottom: 30px;
+  }
+</style>
