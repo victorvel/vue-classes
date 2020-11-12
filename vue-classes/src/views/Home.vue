e50adb7de5b7e230f30f86d6214d461c57cd22ab
Victor Velasco <victorvelasco098@gmail.com>
2020-11-11 18:33:34 -0700

    moved data into js file and made search bar work.

--- vue-classes/src/views/Home.vue
+++ vue-classes/src/views/Home.vue
@@ -5,13 +5,14 @@
             <i class="fas fa-search"></i><input v-model="searchText" />
         </form>
     </div>
-    <ClassData />
+    <ClassData :classes="filteredData" @course-add="courseAdded"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import ClassData from '@/components/ClassList.vue'
+import data from '../assets/class-data';
 
 export default {
   name: 'Home',
@@ -21,9 +22,18 @@ export default {
   data() {
     return {
       searchText: '',
+      data
+    }
+  },
+  methods: {
+    courseAdded(id){
+      console.log('hey!', id);
     }
   },
   computed: {
+    filteredData(){
+      return this.data.filter(item => item.name.includes(this.searchText));
+    }
   }
 }
 </script>
