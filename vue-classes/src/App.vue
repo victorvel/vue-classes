d0e5eed32b9f05cc8f8efa47e01aaa7ca2ac7a26
Victor Velasco <victorvelasco098@gmail.com>
2020-11-12 22:13:33 -0700

    made homework help file

--- vue-classes/src/App.vue
+++ vue-classes/src/App.vue
@@ -2,9 +2,13 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/help">Homework Help</router-link>
     </div>
     <router-view/>
+    <footer>
+      <p>Github link: <a href="https://github.com/victorvel/vue-classes">here.</a></p>
+    </footer>
   </div>
 </template>
 
@@ -28,5 +32,14 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #59bf8e;
+  color: white;
+  text-align: center;
 }
 </style>
